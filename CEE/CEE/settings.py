ad51f79ec0a32434dfa6831bf271670e16f0461b
pandeyramu <pandeyramu57@gmail.com>
2026-05-09 14:19:33 +0545

    Review button added for post submission

--- CEE/CEE/settings.py
+++ CEE/CEE/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # Support both common env var names used in Django deployments.
